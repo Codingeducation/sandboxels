7ca9ee8d55e91b47b578f623337bd0f056296b49
Sagittarius <95559764+Codingeducation@users.noreply.github.com>
2024-09-20 17:19:46 +0800

    Create taylor_swift.js
    Some of the eras. now in powder forms!

---
+++ mods/taylor_swift.js
@@ -0,0 +1,13 @@
+elements.1989 = {
+	color: "#7ccfcd",
+	behavior: behaviors.SUPPORTPOWDER,
+	category: "powders",
+	state: "solid",
+};
+
+elements.folklore = {
+	color: "#d1d1d1",
+	behavior: behaviors.SUPPORTPOWDER,
+	category: "powders",
+	state: "solid",
+};
